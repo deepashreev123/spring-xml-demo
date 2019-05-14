acd00e7b4ca4cc6f570d1ec8330e00001220d4e6
deepashreev123 <deepashree952@gmail.com>
2019-05-14 20:50:49 +0530

    completed task4

--- src/main/java/com/stackroute/springapplication/beans/Movie.java
+++ src/main/java/com/stackroute/springapplication/beans/Movie.java
@@ -1,14 +1,13 @@
 package com.stackroute.springapplication.beans;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+
 public class Movie {
     Actor actor = new Actor();
-
-    public Movie(Actor actor) {
-        this.actor = actor;
-    }
-
-    public Movie() {
-    }
+    private BeanFactory beanFactory;
+    private Object applicationContextAware;
 
     public Actor getActor() {
         return actor;
@@ -17,8 +16,19 @@ public class Movie {
     public void setActor(Actor actor) {
         this.actor = actor;
     }
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException{
+        this.beanFactory=beanFactory;
+    }
+
+    public void setBeanName(String s) {
+        System.out.println("Bean name is "+s);
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContextAware=applicationContextAware;
+    }
+
 }
-
-
 
 
